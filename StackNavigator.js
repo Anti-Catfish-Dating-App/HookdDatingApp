9399a49ceb66b96eef86404f3f7db7a88775f78c
Daniel Schneider <danschneider92@gmail.com>
2021-11-23 17:38:44 -0500

    user login stack navigation

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,14 +3,22 @@ import React from "react"
 import { View, Text } from "react-native"
 import Home from "./screens/Home"
 import Matches from "./screens/Matches"
+import Login from "./screens/Login"
 
 const stack = createNativeStackNavigator()
 
 const StackNavigator = () => {
+  const user = true
   return (
     <stack.Navigator>
-      <stack.Screen name="Home" component={Home} />
-      <stack.Screen name="Matches" component={Matches} />
+      {user ? (
+        <>
+          <stack.Screen name="Home" component={Home} />
+          <stack.Screen name="Matches" component={Matches} />
+        </>
+      ) : (
+        <stack.Screen name="Login" component={Login} />
+      )}
     </stack.Navigator>
   )
 }
