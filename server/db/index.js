7a3d8fec2b2a6c99f3110dda0f5b5bbe680da803
Christian Anderson <somecallmehans@gmail.com>
2021-11-23 12:58:38 -0500

    Association file

--- server/db/index.js
+++ server/db/index.js
@@ -2,14 +2,23 @@ const db = require('./db');
 const Sequelize = require('sequelize');
 
 //Import models
-
+const { Conversations, Images, Matches, Messages, NotMatches, Preferences, Reviews, User } = require('./models');
 
 
 //Associations
-
-
+User.belongsToMany(User, { as: 'swipedOnUser', through: Matches})
+User.belongsToMany(User, { as: 'swiperOnUser', through: NotMatches})
 
 module.exports = {
   db,
-  models: {}
+  models: {
+    Conversations,
+    Images,
+    Matches,
+    Messages,
+    NotMatches,
+    Preferences,
+    Reviews,
+    User
+  }
 }
