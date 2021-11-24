ecfd4483338a7e57a92526ad18bbe921cbebce04
Laura Berkey <berkeylaura@gmail.com>
2021-11-24 09:51:28 -0600

    swiper no swipin

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import React from "react"
 import { View, Text } from "react-native"
 import Home from "./screens/Home"
 import Matches from "./screens/Matches"
+import Login from "./screens/Login"
 import Swipe from "./screens/Swipe"
 
 const stack = createNativeStackNavigator()
