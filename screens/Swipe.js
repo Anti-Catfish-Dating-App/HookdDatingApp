63caa47d3570882cbce69763402d445abb537422
Laura Berkey <berkeylaura@gmail.com>
2021-11-27 15:25:56 -0600

    looking decent

--- screens/Swipe.js
+++ screens/Swipe.js
@@ -1,32 +1,183 @@
 import React, { useEffect, useState } from "react"
-import { useController, useForm } from "react-hook-form"
+
 import {
   StyleSheet,
   Text,
   View,
+  Image,
   SafeAreaView,
   TextInput,
   Alert,
   Button,
   TouchableOpacity,
 } from "react-native"
-import { InputForm } from "./Input"
 import { connect } from "react-redux"
-import { authenticate } from "../store"
 import { useNavigation } from "@react-navigation/native"
+import Swiper from "react-native-deck-swiper"
+import pond from "../store/dummySwipeData"
 
-const Lovers = [
-  { id: "1", uri: require("../assets/dope.jpeg") },
-  { id: "2", uri: require("../assets/heart.jpeg") },
-  { id: "3", uri: require("../assets/manmail.jpeg") },
-]
-
-export default function Swipe(props) {
-  const navigation = useNavigation()
-
+const Card = ({ card }) => {
   return (
-    <View>
-      <Text>Swiper</Text>
+    <View style={styles.currentCard}>
+      <Image source={{ uri: card.pic }} style={styles.cardImage} />
     </View>
   )
 }
+
+const CardDetails = ({ index }) => (
+  <View key={pond[index].id} style={{ alignItems: "center" }}>
+    <Text style={[styles.text, styles.heading]} numberOfLines={2}>
+      {pond[index].first_name}
+    </Text>
+    <Text style={[styles.text, styles.age]}>{pond[index].age}</Text>
+    <Text style={[styles.text, styles.gender]}>{pond[index].gender}</Text>
+    <Text style={[styles.text, styles.bio]}>{pond[index].bio}</Text>
+  </View>
+)
+
+const swiperRef = React.createRef()
+
+export default function Swipe(props) {
+  const navigation = useNavigation()
+  //set initial index
+  const [index, setIndex] = React.useState(0)
+  //Swiper gives this method.
+  const onSwiped = () => {
+    //infinitely go through stack
+    setIndex((index + 1) % pond.length)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.swiperContainer}>
+        <Swiper
+          ref={swiperRef}
+          cards={pond}
+          cardIndex={index}
+          renderCard={(fish) => <Card card={fish} />}
+          onSwiped={onSwiped}
+          //if we want stacking effect but this is giving me issues
+          // stackSize={5}
+          // stackScale={10}
+          // stackSeperation={10}
+          disableTopSwipe
+          disableBottomSwipe
+          animateOverlayLabelsOpacity
+          animateCardOpacity
+          infinite
+          backgroundColor={"transparent"}
+          overlayLabels={{
+            left: {
+              title: "RELEASE",
+              style: {
+                label: {
+                  backgroundColor: "red",
+                  color: "white",
+                  fontSize: 12,
+                },
+                wrapper: {
+                  flexDirection: "column",
+                  alignItems: "flex-end",
+                  justifyContent: "flex-start",
+                  marginTop: 20,
+                  marginLeft: -20,
+                },
+              },
+            },
+            right: {
+              title: "CATCH",
+              style: {
+                label: {
+                  backgroundColor: "green",
+                  color: "white",
+                  fontSize: 12,
+                },
+                wrapper: {
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  justifyContent: "flex-start",
+                  marginTop: 20,
+                  marginLeft: 20,
+                },
+              },
+            },
+          }}
+        />
+      </View>
+
+      <View style={styles.bottomContainer}>
+        {/* swipe right and swip left buttons */}
+        <View style={styles.bottomContainerButtons}>
+          <Button
+            title="<"
+            color={"red"}
+            onPress={() => swiperRef.current.swipeLeft()}
+          />
+          <Button
+            title=">"
+            color={"green"}
+            onPress={() => swiperRef.current.swipeRight()}
+          />
+        </View>
+        <CardDetails index={index} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  swiperContainer: {
+    flex: 0.55,
+  },
+  bottomContainer: {
+    flex: 0.45,
+    justifyContent: "space-evenly",
+  },
+  bottomContainerMeta: { alignContent: "flex-end", alignItems: "center" },
+  bottomContainerButtons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  cardImage: {
+    width: 160,
+    flex: 1,
+    resizeMode: "contain",
+  },
+  currentCard: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: "black",
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 50,
+    backgroundColor: "transparent",
+    fontFamily: "Helvetica",
+  },
+  done: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "white",
+    backgroundColor: "transparent",
+  },
+
+  heading: {
+    fontSize: 32,
+    marginBottom: 10,
+    color: "blue",
+    textAlign: "left",
+    fontWeight: "600",
+  },
+  age: { color: "black", fontSize: 24, fontWeight: "500", textAlign: "left" },
+  bio: { color: "black", fontSize: 20, fontWeight: "200" },
+  gender: { color: "black", fontSize: 14, fontWeight: "200" },
+})
