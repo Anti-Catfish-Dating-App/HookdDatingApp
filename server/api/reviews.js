25731870b9f6585ce8cab3a0939c17396a92be90
Christian Anderson <somecallmehans@gmail.com>
2021-12-01 14:28:57 -0500

    Updated API to findOrCreate to prevent multiple reviews

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -9,17 +9,21 @@ const {
 router.post("/", async (req, res, next) => {
   try {
     const reviewer = await User.findByToken(req.headers);
-    console.log("REVIEWER: ", reviewer.id);
     const { review, reviewedUser } = req.body.reviewInfo
     const rating = parseInt(req.body.reviewInfo.rating)
-    console.log("1-", review);
-    console.log("2-", reviewedUser);
-    console.log("3-", rating);
-    const createdReview = await Reviews.create({
-      rating: rating,
-      reviewText: review,
-      reviewedUser: reviewedUser,
-      reviewer: reviewer.id
+
+
+    const createdReview = await Reviews.findOrCreate({
+      where: {
+        reviewedUser: reviewedUser,
+        reviewer: reviewer.id
+      },
+      defaults: {
+        rating: rating,
+        reviewText: review,
+        reviewedUser: reviewedUser,
+        reviewer: reviewer.id
+      }
     })
 
     res.send(createdReview);
