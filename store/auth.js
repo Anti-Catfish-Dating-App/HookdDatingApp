b368a2bd346368a32b28d8b1176fb9b20005bef8
Christian Anderson <somecallmehans@gmail.com>
2021-12-04 14:39:10 -0500

    Added Heroku links back

--- store/auth.js
+++ store/auth.js
@@ -13,7 +13,7 @@ const editProfile = (user) => ({ type: EDIT_PROFILE, user })
 export const editUser = (user) => async (dispatch) => {
   try {
     const res = await axios.put(
-      `http://192.168.0.6:8080/api/users/${user.id}`,
+      `https://hookd-datingapp.herokuapp.com/api/users/${user.id}`,
       user
     )
     dispatch(editProfile(res.data))
@@ -26,7 +26,7 @@ export const editUser = (user) => async (dispatch) => {
 export const me = () => async (dispatch) => {
   const token = await AsyncStorage.getItem(TOKEN)
   if (token) {
-    const res = await axios.get("http://192.168.0.6:8080/auth/me", {
+    const res = await axios.get("https://hookd-datingapp.herokuapp.com/auth/me", {
       headers: {
         authorization: token,
       },
@@ -39,7 +39,7 @@ export const authenticate =
   (email, password, name, method) => async (dispatch) => {
     try {
       console.log("AUTH", email, password, name)
-      const res = await axios.post(`http://192.168.0.6:8080/auth/${method}`, {
+      const res = await axios.post(`https://hookd-datingapp.herokuapp.com/auth/${method}`, {
         email,
         password,
         name,
