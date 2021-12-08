d4b14bfe254582f6ab165447fbfc2255c70e739b
Daniel Schneider <danschneider92@gmail.com>
2021-12-07 22:34:35 -0500

    user consent styled

--- Hookd-React-Native/screens/UserConsent.js
+++ Hookd-React-Native/screens/UserConsent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useLayoutEffect } from "react"
 import {
   StyleSheet,
   Text,
@@ -10,22 +10,31 @@ import {
 } from "react-native"
 import { connect } from "react-redux"
 import { useNavigation } from "@react-navigation/native"
+import Header from "../components.js/Header"
 
 const UserConsent = (props) => {
   const navigation = useNavigation()
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>User Consent</Text>
-        <Text style={styles.paragraph}>
-          By agreeing, you consent to have your face data stored to be compared
-          with photos in the future. Your baseline photo will not be stored in
-          any database, but your facial data will be stored with Microsoft Face
-          API.
-        </Text>
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
 
-        {/* <Text style={styles.paragraph}>
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Header title="User Consent" />
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>User Consent</Text>
+          <Text style={styles.paragraph}>
+            By agreeing, you consent to have your face data stored to be
+            compared with photos in the future. Your baseline photo will not be
+            stored in any database, but your facial data will be stored with
+            Microsoft Face API.
+          </Text>
+
+          {/* <Text style={styles.paragraph}>
           Hookd will not use your information to sell to any third party. You
           may not use use Hookd to technoloy to develop software, design,
           develop, without Hookd's prior consent. You shall not, nor allow any
@@ -33,42 +42,56 @@ const UserConsent = (props) => {
           software.
         </Text> */}
 
-        <Text style={styles.paragraph}>
-          When you are taking your photo please face forward into the camera and
-          make sure you have good lighting for the AI to see your facial
-          features.
-        </Text>
-        <Button
-          title="I agree"
-          onPress={() => {
-            navigation.navigate("BaselinePhoto")
-          }}
-        />
+          <Text style={styles.paragraph2}>
+            When you are taking your photo please face forward into the camera
+            and make sure you have good lighting for the AI to see your facial
+            features.
+          </Text>
+          <Button
+            title="I agree"
+            onPress={() => {
+              navigation.navigate("BaselinePhoto")
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
   },
   innerContainer: {
     flex: 1,
     alignItems: "center",
-    top: 50,
+    justifyContent: "center",
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
   },
   paragraph: {
-    marginLeft: 30,
-    marginRight: 30,
-    marginBottom: 10,
+    alignContent: "center",
+    textAlign: "center",
+    fontSize: 12,
+    marginBottom: 20,
+    padding: 10,
+  },
+  paragraph2: {
+    alignContent: "center",
+    textAlign: "center",
+    color: "red",
+    fontSize: 15,
+    marginBottom: 20,
+    padding: 10,
   },
 })
 
